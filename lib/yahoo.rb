f5d7fd100da048c4ec8713a72bda114793565d26
pvihtelic <patrick.vihtelic@gmail.com>
2012-12-18 22:29:18 -0600

    adding to yahoo scraper

--- lib/yahoo.rb
+++ lib/yahoo.rb
@@ -18,13 +18,65 @@ class Yahoo
     @url.each do |url|
       analyst_data = Nokogiri::HTML(open(url))
 
-      full_text_name = earnings_data.css("div.title h2").text
+      full_text_name = analyst_data.css("div.title h2").text
       name = full_text_name.gsub(/\([^()]*\)(?![^\[]*])/,"")
       ticker = full_text_name.scan(/\([^()]*\)(?![^\[]*])/).first.gsub("(","").gsub(")","")
 
-      market_data = Nokogiri::HTML(open("http://finance.yahoo.com/q?s=#{ticker}"))
+      market_data = Nokogiri::HTML(open("http://finance.yahoo.com/q/ks?s=#{ticker}"))
+      # puts market_data
+
+      market_cap_text = market_data.xpath('//span[contains(@id, "yfs_j10")]').text
+
+      if market_cap_text.include?("B")
+        market_cap = market_cap_text.gsub("B","").to_i*1000
+      elsif market_cap_text.include?("M")
+        market_cap = market_cap_text.gsub("M","")
+      else
+      end 
+
+      if !market_data.xpath('//tr/td[contains(@class, "yfnc_table")]')[3].nil?
+        @enterprise_value_text = market_data.xpath('//tr/td[contains(@class, "yfnc_table")]')[3].text
+      else
+        @enterprise_value_text = "0" 
+      end
+
+      if @enterprise_value_text.include?("B")
+        enterprise_value = @enterprise_value_text.gsub("B","").to_i*1000
+      elsif @enterprise_value_text.include?("M")
+        enterprise_value = @enterprise_value_text.gsub("M","")
+      else
+      end
+
+      # puts enterprise_value
+
+      # NOT AN EXLPLICIT COLUMN IN THE DATABASE - EV / TTM SALES
+      if !market_data.xpath('//tr/td[contains(@class, "yfnc_table")]')[15].nil?
+        @ev_sales = market_data.xpath('//tr/td[contains(@class, "yfnc_table")]')[15].text
+      else
+        @ev_sales = "0" 
+      end
+
+
+      # NOT AN EXLPLICIT COLUMN IN THE DATABASE - EV / TTM EBITDA
+      if !market_data.xpath('//tr/td[contains(@class, "yfnc_table")]')[17].nil?
+        @ev_ebitda = market_data.xpath('//tr/td[contains(@class, "yfnc_table")]')[17].text
+      else
+        @ev_ebitda = "0" 
+      end
+
+      # puts @ev_ebitda
+
+      cy_sales_text = analyst_data.xpath('//tr/td[contains(@class, "yfnc_table")]')[28].text.gsub("B","").gsub("M","")
       
-      puts name
+      
+      puts cy_sales_text
+
+      # sales_cy_plus_one_text = 
+      # earnings_ltm =
+      # earnings_cy_text = 
+      # earnings_cy_plus_one_text = 
+
+
 
     end
   end
